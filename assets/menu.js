1666be87cf2ed177edcf5f18ff6a20bb4f8c87cd
Rac0n02 <30778293+Rac0n02@users.noreply.github.com>
2018-03-10 14:42:03 +0100

    Add files via upload

---
+++ assets/menu.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    /* "Hjem" i menyen */
+
+    $(".hjem").click(function(){
+        $("#om-container").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#om-container2").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#gear-container").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#index-stream").delay(150).fadeIn(500)
+    });
+    $(".hjem").click(function(){
+        $("#index-instagram").delay(150).show(500)
+    });
+    $(".hjem").click(function(){
+        $("#index-snapchat").delay(150).show(500)
+    });
+    $(".hjem").click(function(){
+        $("#RascoBots-container").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#RascoBots-container2").hide(100)
+    });
+    /* "Om" i menyen */
+
+    $(".om").click(function(){
+        $("#index-stream").hide(100)
+    });
+    $(".om").click(function(){
+        $("#index-instagram").hide(100)
+    });
+    $(".om").click(function(){
+        $("#index-snapchat").hide(100)
+    });
+    $(".om").click(function(){
+        $("#gear-container").hide(100)
+    });
+    $(".om").click(function(){
+        $("#om-container").delay(150).show(500)
+    });
+    $(".om").click(function(){
+        $("#om-container2").delay(150).show(500)
+    });
+    $(".om").click(function(){
+        $("#RascoBots-container").hide(100)
+    });
+    $(".om").click(function(){
+        $("#RascoBots-container2").hide(100)
+    });
+
+   /* RascoBots */
+
+    $(".RascoBots").click(function(){
+        $("#index-stream").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#index-instagram").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#index-snapchat").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#gear-container").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#gear-container").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#om-container").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#om-container2").hide(100)
+    });
+    $(".RascoBots").click(function(){
+        $("#RascoBots-container").delay(150).show(500)
+    });
+    $(".RascoBots").click(function(){
+        $("#RascoBots-container2").delay(150).show(500)
+    });
+
+    /* --- */
+
+    /* "Gear" i menyen */
+
+    $(".gear").click(function(){
+        $("#index-stream").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#index-instagram").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#index-snapchat").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#om-container").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#om-container2").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#gear-container").delay(150).show(500)
+    });
+    $(".gear").click(function(){
+        $("#RascoBots-container").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#RascoBots-container2").hide(100)
+
+    });
+});
