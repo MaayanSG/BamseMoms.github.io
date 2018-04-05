3adda87d220ca148dd8e447bb33337d37e2061d6
Rac0n02 <30778293+Rac0n02@users.noreply.github.com>
2018-04-05 21:41:40 +0200

    fix update

--- assets/menu.js
+++ assets/menu.js
@@ -13,28 +13,10 @@ $(document).ready(function(){
     $(".hjem").click(function(){
         $("#index-stream").delay(150).fadeIn(500)
     });
-    $(".hjem").click(function(){
-        $("#index-instagram").delay(150).show(500)
-    });
-    $(".hjem").click(function(){
-        $("#index-snapchat").delay(150).show(500)
-    });
-    $(".hjem").click(function(){
-        $("#RascoBots-container").hide(100)
-    });
-    $(".hjem").click(function(){
-        $("#RascoBots-container2").hide(100)
-    });
     /* "Om" i menyen */
 
     $(".om").click(function(){
         $("#index-stream").hide(100)
-    });
-    $(".om").click(function(){
-        $("#index-instagram").hide(100)
-    });
-    $(".om").click(function(){
-        $("#index-snapchat").hide(100)
     });
     $(".om").click(function(){
         $("#gear-container").hide(100)
@@ -45,55 +27,12 @@ $(document).ready(function(){
     $(".om").click(function(){
         $("#om-container2").delay(150).show(500)
     });
-    $(".om").click(function(){
-        $("#RascoBots-container").hide(100)
-    });
-    $(".om").click(function(){
-        $("#RascoBots-container2").hide(100)
-    });
 
-   /* RascoBots */
-
-    $(".RascoBots").click(function(){
-        $("#index-stream").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#index-instagram").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#index-snapchat").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#gear-container").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#gear-container").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#om-container").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#om-container2").hide(100)
-    });
-    $(".RascoBots").click(function(){
-        $("#RascoBots-container").delay(150).show(500)
-    });
-    $(".RascoBots").click(function(){
-        $("#RascoBots-container2").delay(150).show(500)
-    });
-
-    /* --- */
 
     /* "Gear" i menyen */
 
     $(".gear").click(function(){
         $("#index-stream").hide(100)
-    });
-    $(".gear").click(function(){
-        $("#index-instagram").hide(100)
-    });
-    $(".gear").click(function(){
-        $("#index-snapchat").hide(100)
     });
     $(".gear").click(function(){
         $("#om-container").hide(100)
@@ -103,12 +42,5 @@ $(document).ready(function(){
     });
     $(".gear").click(function(){
         $("#gear-container").delay(150).show(500)
-    });
-    $(".gear").click(function(){
-        $("#RascoBots-container").hide(100)
-    });
-    $(".gear").click(function(){
-        $("#RascoBots-container2").hide(100)
-
     });
 });
