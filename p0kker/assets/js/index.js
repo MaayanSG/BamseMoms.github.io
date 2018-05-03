634f880f935be62d0a112829e43f66d5870d8c30
Rac0n02 <30778293+Rac0n02@users.noreply.github.com>
2018-05-03 21:29:15 +0200

    P0kker er kul <3

--- p0kker/assets/js/index.js
+++ p0kker/assets/js/index.js
@@ -1,28 +1,46 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function(){
+    /* "Hjem" i menyen */
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    $(".hjem").click(function(){
+        $("#om-container").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#om-container2").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#gear-container").hide(100)
+    });
+    $(".hjem").click(function(){
+        $("#index-stream").delay(150).fadeIn(500)
+    });
+    /* "Om" i menyen */
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    $(".om").click(function(){
+        $("#index-stream").hide(100)
+    });
+    $(".om").click(function(){
+        $("#gear-container").hide(100)
+    });
+    $(".om").click(function(){
+        $("#om-container").delay(150).show(500)
+    });
+    $(".om").click(function(){
+        $("#om-container2").delay(150).show(500)
+    });
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+    /* "Gear" i menyen */
+
+    $(".gear").click(function(){
+        $("#index-stream").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#om-container").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#om-container2").hide(100)
+    });
+    $(".gear").click(function(){
+        $("#gear-container").delay(150).show(500)
+    });
+});
