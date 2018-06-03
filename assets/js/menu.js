fa65e3bd3f678b6a02a20803608103a5e6092b82
BamseMoms <30778293+BamseMoms@users.noreply.github.com>
2018-06-03 16:58:03 +0200

    ohhhhh 2018

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,21 +8,15 @@ $(document).ready(function(){
         $("#om-container2").hide(100)
     });
     $(".hjem").click(function(){
-        $("#om-container3").hide(100)
-    });
-    $(".hjem").click(function(){
-        $("#om-container4").hide(100)
-    });
-    $(".hjem").click(function(){
         $("#gear-container").hide(100)
     });
     $(".hjem").click(function(){
-        $("#index-stream").delay(150).fadeIn(500)
+        $("#index").delay(150).fadeIn(500)
     });
     /* "Om" i menyen */
 
     $(".om").click(function(){
-        $("#index-stream").hide(100)
+        $("#index").hide(100)
     });
     $(".om").click(function(){
         $("#gear-container").hide(100)
@@ -33,30 +27,17 @@ $(document).ready(function(){
     $(".om").click(function(){
         $("#om-container2").delay(150).show(500)
     });
-    $(".om").click(function(){
-        $("#om-container3").delay(150).show(500)
-    });
-    $(".om").click(function(){
-        $("#om-container4").delay(150).show(500)
-    });
-
 
     /* "Gear" i menyen */
 
     $(".gear").click(function(){
-        $("#index-stream").hide(100)
+        $("#index").hide(100)
     });
     $(".gear").click(function(){
         $("#om-container").hide(100)
     });
     $(".gear").click(function(){
         $("#om-container2").hide(100)
-    });
-    $(".gear").click(function(){
-        $("#om-container3").hide(100)
-    });
-    $(".gear").click(function(){
-        $("#om-container4").hide(100)
     });
     $(".gear").click(function(){
         $("#gear-container").delay(150).show(500)
