12148fdd6b69afdec7d083e562cdb8890d2a6304
BamseMoms <30778293+BamseMoms@users.noreply.github.com>
2019-02-23 16:23:06 +0100

    bug added giveaway on timer

--- js/main.js
+++ js/main.js
@@ -78,8 +78,11 @@ $(document).ready(function() {
         }
       }
     );
+
+    // TIMER, SHOW GIVEAWAY HTML AFTER TIMER IS DONE //
+
     // Set the date we're counting down to
-    var countDownDate = new Date("jan 1, 2020 00:00:00").getTime();
+    var countDownDate = new Date("feb 23, 2090 16:12:00").getTime();
     // Update the count down every 1 second
     var countdownfunction = setInterval(function() {
     // Get todays date and time
@@ -91,18 +94,48 @@ $(document).ready(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Output the result in an element with id="timer"
-    document.getElementById("tid").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+
+    // Output the result in an element with id="tid"
+    //document.getElementById("giveaway").innerHTML = days + "d " + hours + "h "
+   // + minutes + "m " + seconds + "s ";
+
     // If the count down is over, write some text 
     if (distance < 0) {
     clearInterval(countdownfunction);
-     document.getElementById("tid").innerHTML = "VI ER I 2020!";
+     document.getElementById("giveaway").innerHTML = '<div style=" text-align: center; border: 5px solid #111; border-radius: 4px; background: #2c3e50; background: -webkit-linear-gradient(to right, #BD3F32, #CB356B);"><h1> GIVEAWAY </h1><p style=" font-size: 18px; "> Det er giveaway på <span style="color: rgb(46, 127, 232)">500KR </span><br><p><a href="https://twitch.tv/BamseMoms" style=" ont-weight: bold; color: rgb(46, 127, 232); font-size: 30px;">KLIKK HER</a></p></div>';
     }
     }, 0);
+       
+         })();
+         
+         
+       });
+       // TIMER, COUNTDOWN TO WE ARE IN 2020 //
+    // Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
     
-      })();
-      
-      
-    });
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
     
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="tid"
+  document.getElementById("tid").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("tid").innerHTML = "VI ER I 2020!";
+  }
+}, 1000);
